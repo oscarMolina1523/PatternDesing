d675eb216d1ceab87d6e99c56b28d4bd4d8854d7
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-15 16:15:39 -0600

    adding an scenary to do an example

--- src/patterns/PatternFactory.ts
+++ src/patterns/PatternFactory.ts
@@ -1,3 +1,8 @@
 //Explaining the factory pattern, Easy to understand
 //The goal is not to depend on a single class but to pass
-//  it the implementation only and the rest will be done by itself.
+//it the implementation only and the rest will be done by itself.
+// scenery: You want a system that sends notifications. These can be:
+// Email
+// SMS
+// Push notification
+// The client doesn't need to worry about what type of notification it is; they'll just call a method to send it.

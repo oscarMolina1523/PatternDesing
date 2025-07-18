cbe4424ba378248c9dd525be43dc95c2f31be337
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-18 13:27:34 -0600

    adding class of sony

--- src/patterns/Bridge.ts
+++ src/patterns/Bridge.ts
@@ -6,10 +6,11 @@ interface Device {
   setChannel(channel: number): void;
 }
 
+//this class implements the Device interface for a Samsung basic TV
 class SamsungTV implements Device {
-  // Propiedad 'enabled' indica si está encendida o apagada
+  // property to know if is on or off
   private enabled = false;
-  // Propiedad 'channel' indica el canal actual
+  // property to know the current channel
   private channel = 1;
 
   isEnabled() { return this.enabled; }
@@ -19,4 +20,18 @@ class SamsungTV implements Device {
   disable() { this.enabled = false; console.log("Samsung TV apagada"); }
 
   setChannel(channel: number) { this.channel = channel; console.log(`Samsung TV canal ${channel}`); }
+}
+
+//this class implements the Device interface for a Sony basic TV
+class SonyTV implements Device {
+  private enabled = false;
+  private channel = 1;
+
+  isEnabled() { return this.enabled; }
+
+  enable() { this.enabled = true; console.log("Sony TV encendida"); }
+
+  disable() { this.enabled = false; console.log("Sony TV apagada"); }
+
+  setChannel(channel: number) { this.channel = channel; console.log(`Sony TV canal ${channel}`); }
 }

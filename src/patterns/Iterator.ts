0561312b418a097314db94721a715b840eb72ae6
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-21 12:13:06 -0600

    exporting class wordsCollection to test in index

--- src/patterns/Iterator.ts
+++ src/patterns/Iterator.ts
@@ -16,7 +16,7 @@ interface Aggregator {
 //my collection that implements the aggregator interface
 //this collection is only a simple example of a collection of words
 //you can implement it with any type of data
-class WordsCollection implements Aggregator {
+export class WordsCollection implements Aggregator {
     //static collection of words
   private items: string[] = [];
 

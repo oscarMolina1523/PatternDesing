38128ea4f7f4d05e34f9b109834d3d2be373fb8a
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-22 09:18:05 -0600

    creating a class user with methods control for chat

--- src/patterns/Mediator.ts
+++ src/patterns/Mediator.ts
@@ -3,3 +3,17 @@ interface ChatRoomMediator {
   sendMessage(message: string, user: User): void;
   addUser(user: User): void;
 }
+
+
+class User {
+  constructor(private name: string, private chatRoom: ChatRoomMediator) {}
+
+  send(message: string) {
+    console.log(`${this.name} envía: ${message}`);
+    this.chatRoom.sendMessage(message, this);
+  }
+
+  receive(message: string) {
+    console.log(`${this.name} recibe: ${message}`);
+  }
+}

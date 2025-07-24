b52b3825a6749d4c6938c7d03b4028ee5a2447ae
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-23 19:38:37 -0600

    feat: implement ConcreteBurgerBuilder and BurgerDirector for burger construction

--- src/patterns/Builder.ts
+++ src/patterns/Builder.ts
@@ -25,3 +25,84 @@ interface BurgerBuilder {
   setTamaño(tamaño: string): void;
   getBurger(): Burger;
 }
+
+// 🍔 Concrete Builder that implements the BurgerBuilder interface
+class ConcreteBurgerBuilder implements BurgerBuilder {
+    // 🍔 this is the Burger that we are building
+  private burger: Burger;
+
+  constructor() {
+    // 🍔 Initialize a new Burger instance
+    this.reset();
+  }
+
+  // 🍔 Reset the builder to start a new Burger
+  public reset(): void {
+    this.burger = new Burger();
+  }
+
+  // 🍔 Methods to add parts to the Burger
+  public addPan(): void {
+    this.burger.addPart("Pan");
+  }
+
+  public addCarne(): void {
+    this.burger.addPart("Carne");
+  }
+
+  public addQueso(): void {
+    this.burger.addPart("Queso");
+  }
+
+  public addLechuga(): void {
+    this.burger.addPart("Lechuga");
+  }
+
+  public addTomate(): void {
+    this.burger.addPart("Tomate");
+  }
+
+  public addSalsa(salsa: string): void {
+    this.burger.addPart(`Salsa ${salsa}`);
+  }
+
+  public setTamaño(tamaño: string): void {
+    this.burger.addPart(`Tamaño ${tamaño}`);
+  }
+
+  // 🍔 Get the constructed Burger and reset the builder for a new one
+  public getBurger(): Burger {
+    const result = this.burger;
+    this.reset(); // reset the builder for a new Burger
+    return result;
+  }
+}
+
+// 👨‍💼 Director: define secuence of construction
+class BurgerDirector {
+  private builder: BurgerBuilder;
+
+  //receives the builder to use
+  public setBuilder(builder: BurgerBuilder): void {
+    this.builder = builder;
+  }
+
+  // 🍔 Methods to build cheese burger , its a usually combination
+  public buildCheeseBurger(): void {
+    this.builder.addPan();
+    this.builder.addCarne();
+    this.builder.addQueso();
+    this.builder.addLechuga();
+    this.builder.setTamaño("Grande");
+    this.builder.addSalsa("BBQ");
+  }
+
+  // 🍔 Methods to build veggie burger , its a usually combination
+  public buildVeggieBurger(): void {
+    this.builder.addPan();
+    this.builder.addLechuga();
+    this.builder.addTomate();
+    this.builder.setTamaño("Mediana");
+    this.builder.addSalsa("Mostaza");
+  }
+}

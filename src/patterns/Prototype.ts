a4e351d3dc0d92ae33d0505f072805b4d832dd40
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-25 13:44:45 -0600

    refactor: update clone method return type to BurgerNormal for clarity

--- src/patterns/Prototype.ts
+++ src/patterns/Prototype.ts
@@ -2,3 +2,19 @@
 interface BurgerPrototype {
   clone(): BurgerPrototype;
 }
+
+
+// 🔷 Concrete class
+class BurgerNormal implements BurgerPrototype {
+  constructor(
+    public pan: string,
+    public carne: string,
+    public queso: string,
+    public salsa: string
+  ) {}
+
+  // clone method to create a copy of the burger
+  clone(): BurgerNormal {
+    return new BurgerNormal(this.pan, this.carne, this.queso, this.salsa);
+  }
+}

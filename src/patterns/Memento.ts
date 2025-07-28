d184a6389a3d80e494d53a2c22f6bed03f4717d1
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-28 11:54:23 -0600

    docs: solving dude why no implement this pattern

---
+++ src/patterns/Memento.ts
@@ -0,0 +1,4 @@
+//debido a que no se le ha encontrado utilidad a este patron en el proyecto, se ha decidido no implementarlo
+//por lo que se ha dejado el archivo vacio, pero se ha mantenido para no perder el contexto del proyecto
+//es decir solo le veo utilidad para cosas como retroceder en archivos de texto como word y asi
+//no le veo utilidad en un proyecto mas real

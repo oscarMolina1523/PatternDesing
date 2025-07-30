38dd7d0acf2d41066ed0dac246f62456fd276d08
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-30 09:15:16 -0600

    feat: create a music player for the context

--- src/patterns/State.ts
+++ src/patterns/State.ts
@@ -3,3 +3,29 @@ interface PlayerState {
   pause(): void;
   stop(): void;
 }
+
+class MusicPlayer {
+  private state!: PlayerState;
+
+  constructor(initialState: PlayerState) {
+    this.setState(initialState);
+  }
+
+  public setState(state: PlayerState) {
+    console.log(`🎵 Estado actual: ${state.constructor.name}`);
+    this.state = state;
+    this.state.setContext(this);
+  }
+
+  public play() {
+    this.state.play();
+  }
+
+  public pause() {
+    this.state.pause();
+  }
+
+  public stop() {
+    this.state.stop();
+  }
+}

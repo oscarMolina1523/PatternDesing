a97c5a55e6686d380de4d36a8f125df341788e27
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-31 09:19:35 -0600

    creating concretes strategies

--- src/patterns/Strategy.ts
+++ src/patterns/Strategy.ts
@@ -2,3 +2,22 @@
 interface ShippingStrategy {
   calculate(orderTotal: number): number;
 }
+
+// 2. Estrategias concretas
+class StandardShipping implements ShippingStrategy {
+  calculate(orderTotal: number): number {
+    return orderTotal + 5;
+  }
+}
+
+class ExpressShipping implements ShippingStrategy {
+  calculate(orderTotal: number): number {
+    return orderTotal + 15;
+  }
+}
+
+class FreeShipping implements ShippingStrategy {
+  calculate(orderTotal: number): number {
+    return orderTotal + 0;
+  }
+}

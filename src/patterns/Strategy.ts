087131d49ef972773e1e99d3662891afc2a494fb
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-31 09:21:29 -0600

    feat: create a context strategy

--- src/patterns/Strategy.ts
+++ src/patterns/Strategy.ts
@@ -21,3 +21,22 @@ class FreeShipping implements ShippingStrategy {
     return orderTotal + 0;
   }
 }
+
+// 3. Contexto que usa la estrategia
+class ShippingCostCalculator {
+    //recibe una estrategia de envío que implemente ShippingStrategy
+  // y la almacena en una propiedad privada
+  private strategy: ShippingStrategy;
+
+  constructor(strategy: ShippingStrategy) {
+    this.strategy = strategy;
+  }
+
+  setStrategy(strategy: ShippingStrategy) {
+    this.strategy = strategy;
+  }
+
+  calculateCost(orderTotal: number): number {
+    return this.strategy.calculate(orderTotal);
+  }
+}
